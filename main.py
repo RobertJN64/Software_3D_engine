1ffd188556b704ad068d73a30e89f8fc7b361734
RobertJN <robertjnies@gmail.com>
2022-05-18 21:25:42 -0400

    support for python turtle

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 from object_3d import *
 from camera import *
 from projection import *
-import pygame as pg
+#import pygame as pg
+import turtle
+from time import sleep
 
 
 class SoftwareRender:
     def __init__(self):
-        pg.init()
-        self.RES = self.WIDTH, self.HEIGHT = 1600, 900
+        self.s = turtle.Screen()
+        self.t = turtle.Turtle()
+        turtle.speed(0)
+        turtle.pensize(1)
+        turtle.color('orange')
+        self.WIDTH = self.s.window_width()
+        self.HEIGHT = self.s.window_height()
+        #pg.init()
+        self.RES = self.WIDTH, self.HEIGHT
         self.H_WIDTH, self.H_HEIGHT = self.WIDTH // 2, self.HEIGHT // 2
-        self.FPS = 60
-        self.screen = pg.display.set_mode(self.RES)
-        self.clock = pg.time.Clock()
-        self.create_objects()
+        #self.FPS = 60
+        #self.screen = pg.display.set_mode(self.RES)
+        #self.clock = pg.time.Clock()
 
-    def create_objects(self):
         self.camera = Camera(self, [-5, 6, -55])
         self.projection = Projection(self)
         self.object = self.get_object_from_file('resources/t_34_obj.obj')
@@ -32,17 +39,19 @@ class SoftwareRender:
         return Object3D(self, vertex, faces)
 
     def draw(self):
-        self.screen.fill(pg.Color('darkslategray'))
-        self.object.draw()
+        self.s.bgcolor('darkgrey')
+        self.s.tracer(10000)
+        self.object.draw(self.t)
+        self.s.update()
+        sleep(1)
+        self.s.clear()
+
+
 
     def run(self):
         while True:
             self.draw()
             self.camera.control()
-            [exit() for i in pg.event.get() if i.type == pg.QUIT]
-            pg.display.set_caption(str(self.clock.get_fps()))
-            pg.display.flip()
-            self.clock.tick(self.FPS)
 
 
 if __name__ == '__main__':
--- object_3d.py
+++ object_3d.py
@@ -1,4 +1,4 @@
-import pygame as pg
+#import pygame as pg
 from matrix_functions import *
 from numba import njit
 
@@ -15,20 +15,22 @@ class Object3D:
         self.faces = np.array([np.array(face) for face in faces])
         self.translate([0.0001, 0.0001, 0.0001])
 
-        self.font = pg.font.SysFont('Arial', 30, bold=True)
-        self.color_faces = [(pg.Color('orange'), face) for face in self.faces]
+        #self.font = pg.font.SysFont('Arial', 30, bold=True)
+        self.color_faces = [('orange', face) for face in self.faces]
         self.movement_flag, self.draw_vertexes = True, False
         self.label = ''
+        self.counter = 0
 
-    def draw(self):
-        self.screen_projection()
+    def draw(self, turtle):
+        self.screen_projection(turtle)
         self.movement()
 
     def movement(self):
+        self.counter += 1
         if self.movement_flag:
-            self.rotate_y(-(pg.time.get_ticks() % 0.005))
+            self.rotate_y(0.5)
 
-    def screen_projection(self):
+    def screen_projection(self, turtle):
         vertexes = self.vertexes @ self.render.camera.camera_matrix()
         vertexes = vertexes @ self.render.projection.projection_matrix
         vertexes /= vertexes[:, -1].reshape(-1, 1)
@@ -36,19 +38,29 @@ class Object3D:
         vertexes = vertexes @ self.render.projection.to_screen_matrix
         vertexes = vertexes[:, :2]
 
+
         for index, color_face in enumerate(self.color_faces):
             color, face = color_face
             polygon = vertexes[face]
-            if not any_func(polygon, self.render.H_WIDTH, self.render.H_HEIGHT):
-                pg.draw.polygon(self.render.screen, color, polygon, 1)
-                if self.label:
-                    text = self.font.render(self.label[index], True, pg.Color('white'))
-                    self.render.screen.blit(text, polygon[-1])
 
-        if self.draw_vertexes:
-            for vertex in vertexes:
-                if not any_func(vertex, self.render.H_WIDTH, self.render.H_HEIGHT):
-                    pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 2)
+            if not any_func(polygon, self.render.H_WIDTH, self.render.H_HEIGHT):
+                turtle.penup()
+                turtle.goto(polygon[0][0] - self.render.H_WIDTH, -polygon[0][1] + self.render.H_HEIGHT)
+                turtle.pendown()
+                for x, y in polygon:
+                    turtle.goto(x - self.render.H_WIDTH, -y + self.render.H_HEIGHT)
+                turtle.goto(polygon[0][0] - self.render.H_WIDTH, -polygon[0][1] + self.render.H_HEIGHT)
+                turtle.penup()
+                #pg.draw.polygon(self.render.screen, color, polygon, 1)
+                #print(polygon)
+                # if self.label:
+                #     text = self.font.render(self.label[index], True, pg.Color('white'))
+                #     self.render.screen.blit(text, polygon[-1])
+
+        # if self.draw_vertexes:
+        #     for vertex in vertexes:
+        #         if not any_func(vertex, self.render.H_WIDTH, self.render.H_HEIGHT):
+        #             pg.draw.circle(self.render.screen, pg.Color('white'), vertex, 2)
 
     def translate(self, pos):
         self.vertexes = self.vertexes @ translate(pos)
@@ -71,7 +83,7 @@ class Axes(Object3D):
         super().__init__(render)
         self.vertexes = np.array([(0, 0, 0, 1), (1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
         self.faces = np.array([(0, 1), (0, 2), (0, 3)])
-        self.colors = [pg.Color('red'), pg.Color('green'), pg.Color('blue')]
+        self.colors = ['red', 'green', 'blue']
         self.color_faces = [(color, face) for color, face in zip(self.colors, self.faces)]
         self.draw_vertexes = False
         self.label = 'XYZ'
